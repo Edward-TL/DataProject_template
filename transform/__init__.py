4709632a846a1e319d732184f4b9611147e94a3a
Edward <edward_tl@hotmail.com>
2024-11-13 16:51:04 -0600

    [FEAT] Transform
    cleaning functions for common tasks

---
+++ transform/__init__.py
@@ -0,0 +1,2 @@
+from transform.pandas_tools import sells_pivot, month_filter, replace_many_texts, replace_text
+from transform.wix_cleaning import currency_clean
---
+++ transform/helpers.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from dotenv import dotenv_values
+from os import listdir
+
+def parent_dir() -> str:
+    """Return the parent directory, so the folder can be added as needed"""
+    return str(Path("./").parent.absolute()).replace("\\transform","")
+
+def get_config(path: str) -> dict:
+    """Given the path of the .env file, file return the values as a dictionary"""
+    return dotenv_values(path)
+
+
+def get_excels(path: str) -> list[str]:
+    """From a path, returns only the files that ends with .xlsx"""
+    return [file for file in listdir(path) if file.endswith(".xlsx")]
+
+# if __name__ == "__main__":
+    # print(MAIN_DIR)
